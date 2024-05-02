972e2ac2da8b5641a03beaf94f5a2b6aff77c859
QIAF <qonitaafifah3@gmail.com>
2024-05-02 22:36:07 +0700

    nextJs - React

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return (
+        <main className="not-found">
+            <h1>Meals Not Found</h1>
+            <p>Unfortunetely, we could not find requested or try again later</p>
+        </main>
+    );
+}
---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunetely, we could not find requested or try again later</p>
+        </main>
+    );
+}
